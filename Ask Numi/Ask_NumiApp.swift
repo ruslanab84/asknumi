9f2929d11927c06823b055b66c2fb5bb6bf34263
Ruslan Abdulov <rusikabdulov@gmail.com>
2026-07-12 10:31:28 +0400

    Initial Commit

---
+++ Ask Numi/Ask_NumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ask_NumiApp.swift
+//  Ask Numi
+//
+//  Created by Ruslan Abdulov on 12.07.26.
+//
+
+import SwiftUI
+
+@main
+struct Ask_NumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ask Numi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ask Numi
+//
+//  Created by Ruslan Abdulov on 12.07.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
